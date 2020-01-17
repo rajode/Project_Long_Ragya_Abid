faa32a87c1d40fd83dc0311088df4fa190e106fe
Long Tran <long.tran2@ucalgary.ca>
2020-01-17 16:39:36 -0700

    First student's task

--- converter.java
+++ converter.java
@@ -1,21 +1,24 @@
 public class Converter {
 //Your names go here:
 /*
-* @Author: Name of the first student
+* @Author: Long Tran
 * Name of the second student
 * Abid Al Labib
 *
 */
 private double celsiusToFahrenheit(double C){
-return ((C* (9/5)) + 32);
+return ((C* 9/5) + 32);
 }
 private double fahrenheitToCelsius(double F){
  // TODO: The second student will implement this method
 return 0;
 }
-public static void main(String[] args) {
-//TODO: The first student will implement this method.
-// Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
-// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
+    public static void main(String[] args) {
+        Converter myObject= new Converter();
+        double fahrenheit=myObject.celsiusToFahrenheit(180);
+        System.out.println(fahrenheit);
+        double celsius=myObject.fahrenheitToCelsius(250);
+        System.out.println(celsius);
+    }
 }
-}
+
